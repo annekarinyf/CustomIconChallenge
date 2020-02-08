f72727c3b8740443fd9069a228a9e0942733ef62
Anne Kariny Silva Freitas <karinyfreitas.1994@gmail.com>
2020-02-08 11:55:06 -0300

    Create Reachability class to check network connection

---
+++ CustomIconChallenge/CustomIconChallenge/Source/Networking/Reachability.swift
@@ -0,0 +1,36 @@
+//
+//  Reachability.swift
+//  CustomIconChallenge
+//
+//  Created by Anne Kariny Silva Freitas on 08/02/20.
+//  Copyright © 2020 Anne Kariny Silva Freitas. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+class Reachability {
+    
+    // MARK: - Internet Connection Verification
+    static var isConnectedToNetwork: Bool {
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+          $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+            SCNetworkReachabilityCreateWithAddress(nil, $0)
+          }
+        }) else {
+          return false
+        }
+
+        var flags = SCNetworkReachabilityFlags(rawValue: 0)
+        guard SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) else { return false }
+        
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        
+        return isReachable && !needsConnection
+    }
+}
