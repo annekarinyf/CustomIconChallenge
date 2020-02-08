a55e133f23dfc41d9433e5ea3f408f98bf8b8a09
Anne Kariny Silva Freitas <karinyfreitas.1994@gmail.com>
2020-02-08 15:15:35 -0300

    Show icon's list on tableView

--- CustomIconChallenge/CustomIconChallenge/Source/Views/Custom Icons/CustomIconsViewController.swift
+++ CustomIconChallenge/CustomIconChallenge/Source/Views/Custom Icons/CustomIconsViewController.swift
@@ -9,10 +9,56 @@
 import UIKit
 
 class CustomIconsViewController: UIViewController {
-
+    @IBOutlet private weak var customIconsTableView: UITableView!
+    private let cellID = "IconTableViewCell"
+    private var iconsViewModel = [IconViewModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        customIconsTableView.register(UINib(nibName: cellID, bundle: nil), forCellReuseIdentifier: cellID)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getIconsFromAPI()
+    }
+    
+    private func getIconsFromAPI() {
+        CustomIconAPIManager.shared.listCustomIcons { [weak self] (icons, error) in
+            guard let strongSelf = self, let icons = icons else { return }
+            
+            strongSelf.iconsViewModel = icons.map { IconViewModel(icon: $0) }
+            strongSelf.customIconsTableView.reloadData()
+        }
     }
 }
+
+// MARK: - UITableView Methods
+extension CustomIconsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return iconsViewModel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard
+            iconsViewModel.indices.contains(indexPath.row),
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as? IconTableViewCell else {
+                return UITableViewCell()
+        }
+        
+        let iconViewModel = iconsViewModel[indexPath.row]
+        cell.setupCell(with: iconViewModel)
+        
+        iconViewModel.getImage { image in
+            cell.setImage(image)
+        }
+    
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 76
+    }
+}
+
 
